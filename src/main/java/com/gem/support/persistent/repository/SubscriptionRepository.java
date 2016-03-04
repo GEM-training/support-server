cabb3976132d5219c593d13f66ba57854e8f356b
cloudteddy <mr.trankimtung@gmail.com>
2016-03-04 16:56:29 +0700

    [Billing] add SubscriptionRepository

--- src/main/java/com/gem/support/persistent/repository/SubscriptionRepository.java
+++ src/main/java/com/gem/support/persistent/repository/SubscriptionRepository.java
@@ -4,6 +4,8 @@ package com.gem.support.persistent.repository;
 import com.gem.support.persistent.model.Subscription;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubscriptionRepository extends PagingAndSortingRepository<Subscription, String>, QueryDslPredicateExecutor<Subscription> {
 }
