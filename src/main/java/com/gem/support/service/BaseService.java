582c83a385793a36dd590f207a997211fdcf67b0
cloudteddy <mr.trankimtung@gmail.com>
2016-03-04 14:40:31 +0700

    [Billing] change return type

--- src/main/java/com/gem/support/service/BaseService.java
+++ src/main/java/com/gem/support/service/BaseService.java
@@ -1,6 +1,6 @@
 package com.gem.support.service;
 
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 
@@ -10,6 +10,6 @@ public interface BaseService<DTO, ID extends Serializable> {
     void update(DTO dto);
     void delete(ID id);
     DTO findOne(ID id);
-    Page<DTO> findAll();
+    DTO findAll(Pageable pageable);
 
 }
