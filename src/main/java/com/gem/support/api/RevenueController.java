1273c0893211582a6e8463362e1c7d564a83192e
nguyenpk57 <nguyenkhoipham1410@gmail.com>
2016-03-08 10:32:56 +0700

    [Billing] add api revenue support caculator revenue by company by time(from-to).

---
+++ src/main/java/com/gem/support/api/RevenueController.java
@@ -0,0 +1,29 @@
+package com.gem.support.api;
+
+import com.gem.support.service.RevenueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by qsoft on 3/7/16.
+ */
+@RestController
+@RequestMapping("/billing/revenue")
+public class RevenueController  {
+    @Autowired
+    private  RevenueService revenueService;
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+    public Double getRevenue(
+            @RequestParam(name = "companyId", required = false) String companyId,
+            @RequestParam(name = "from", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date from,
+            @RequestParam(name = "to", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date to) {
+        return revenueService.getRevenue(companyId, from, to);
+    }
+}
+
